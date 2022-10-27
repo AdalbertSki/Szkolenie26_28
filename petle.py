f957357af3c91f0a3d52ab182326f066f96000c7
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-27 10:53:36 +0200

    if

--- petle.py
+++ petle.py
@@ -20,7 +20,7 @@ else:
 
 print(f"zapłacisz {zarobki * podatek} zł")
 
-zarobki = int(input("Proszę, wprowadź swój dochód: ")) #zmiana na int do wprowadzenia cyfr
+zarobki = int(input("Proszę, wprowadź swój dochód: "))  # zmiana na int do wprowadzenia cyfr
 # < > <= >= != ==
 
 if zarobki < 10000:
@@ -33,4 +33,5 @@ else:
     podatek = 0.40
 
 print(f"zapłacisz {zarobki * podatek} zł")
+
 
