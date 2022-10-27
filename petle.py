7b8848cd381a615df3646433699f6044c7f14df9
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-27 10:27:02 +0200

    if

--- petle.py
+++ petle.py
@@ -1,7 +1,35 @@
-czy_znasz_python=False            # ctrl-alt-l porządkuje kod robi spacje
-if czy_znasz_python:              # ":" robi wcięcie 4 spacje
+czy_znasz_python = False  # ctrl-alt-l porządkuje kod robi spacje
+if czy_znasz_python:  # ":" robi wcięcie 4 spacje
     print("Brawo!")
 else:
     print("Musisz_sie_uczyc_dalej")
 
 print("Ja jestem poza warunkiem")
+
+# < > <= >= != ==
+
+zarobki = 1500
+if zarobki < 10000:
+    podatek = 0.0
+elif zarobki < 3000:
+    podatek = 0.2
+elif zarobki < 100000:
+    podatek = 0.35
+else:
+    podatek = 0.40
+
+print(f"zapłacisz {zarobki * podatek} zł")
+
+zarobki = int(input("Proszę, wprowadź swój dochód: "))
+# < > <= >= != ==
+
+if zarobki < 10000:
+    podatek = 0.0
+elif zarobki < 30000:
+    podatek = 0.2
+elif zarobki < 100000:
+    podatek = 0.35
+else:
+    podatek = 0.40
+
+print(f"zapłacisz {zarobki * podatek} zł")
