e6fc3eee441422adc0b20b3f4317d76f21adee46
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-27 10:08:37 +0200

    od#showałem 3 linie

--- petle.py
+++ petle.py
@@ -1,5 +1,5 @@
-czy_znasz_python=False  # ctrl-alt-l porządkuje kod robi spacje
-if czy_znasz_python:  # ":" robi wcięcie 4 spacje
+czy_znasz_python=False            # ctrl-alt-l porządkuje kod robi spacje
+if czy_znasz_python:              # ":" robi wcięcie 4 spacje
     print("Brawo!")
 else:
     print("Musisz_sie_uczyc_dalej")
