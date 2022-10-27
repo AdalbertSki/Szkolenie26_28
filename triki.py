d29ff26913853fc41fa551d66442f3e434c00028
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-27 13:48:53 +0200

    while

--- triki.py
+++ triki.py
@@ -9,3 +9,18 @@ print({**d, **e})
 print(s.removeprefix("Hello"))
 print(s.removesuffix("There"))
 print(s.removeprefix("Python"))
+
+wybor = input("Napisz swoją ulubioną liczbę: ")
+
+match wybor:
+    case "1":
+        print("Twoja ulubiona cyfra to", wybor)
+    case "3":
+        print("Twoja ulubiona cyfra to", wybor)
+    case "5":
+        print("Twoja ulubiona cyfra to", wybor)
+    case "4":
+        print("Twoja ulubiona cyfra to", wybor)
+    case _:
+        print("**Twoja ulubiona cyfra to**", wybor)
+
