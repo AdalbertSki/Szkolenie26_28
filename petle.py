671a96a63a58bb2320faf4513827fc3a90eea28a
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-27 11:01:33 +0200

    if

--- petle.py
+++ petle.py
@@ -34,4 +34,39 @@ else:
 
 print(f"zapłacisz {zarobki * podatek} zł")
 
+lista_bledow = []
 
+alert_system = 'email'
+error = 'medium'
+error_message = 'Stało się coś strasznego!'
+
+if alert_system == 'console':
+    print(error_message)
+elif alert_system == 'email':
+    if error == 'critical':
+        lista_bledow.append('critical')
+    elif error == 'medium':
+        lista_bledow.append('medium')
+    else:
+        lista_bledow.append('nieznany')
+
+print(lista_bledow)
+
+suma_zam = 247
+# if suma_zam > 100:
+#     rabat = 25
+# else:
+#     rabat = 0
+
+rabat = 25 if suma_zam > 100 else 0
+
+print("SUMA:", suma_zam, "RABAT:", rabat)
+
+lista = [j for j in range(10) if j % 2 == 0]
+print(lista)
+
+lista = []
+for i in range(10):
+    if i % 2 == 0:
+        lista.append(i)
+print(lista)
