7a2a5d97b46d030c23173571eb5074a0ae06b862
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-28 12:39:08 +0200

    xml

---
+++ jsonek.py
@@ -0,0 +1,21 @@
+import json
+
+person = '{"name": "Bob", "languages": ["English", "French", ["Python", "Java"]]}'
+person_dict = json.loads(person)
+
+print(person_dict)
+print(person_dict['languages'])
+print(person_dict['languages'][0])
+print(person_dict['languages'][1])
+print(person_dict['languages'][2][0])
+
+with open('person.json', 'r') as f:
+    data = json.load(f)
+print(data)
+
+
+
+
+
+
+
