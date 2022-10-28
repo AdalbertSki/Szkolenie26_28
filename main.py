2b52d639a80d62bb3a4148a272e7c8d0159987ce
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-28 14:41:34 +0200

    klasy

--- main.py
+++ main.py
@@ -10,22 +10,22 @@
 
 from piatek import klasa1 as k1
 
-dom1 = k1.Dom()
-print(dom1)
+# dom1 = k1.Dom()
+# print(dom1)
 # print(dom1.metraz)
 # dom1.okresl_metraz()
 # print(dom1.metraz)
-dom1.okresl_kolor()
+#dom1.okresl_kolor()
 
 # dom2 = k1.Dom()
 # print(dom2)
 # dom2.okresl_kolor()
 # print("Dom2 ma kolor:", dom2.kolor)
 
-dom3 = k1.Dom2(100, "czerwony", 3)
-print(dom3.metraz)
-print(dom3.ilosc_okien)
-print(dom3.kolor)
-dom3.zmien_okna()
-print(dom3.ilosc_okien)
+# dom3 = k1.Dom2(100, "czerwony", 3)
+# print(dom3.metraz)
+# print(dom3.ilosc_okien)
+# print(dom3.kolor)
+# dom3.zmien_okna()
+# print(dom3.ilosc_okien)
 
