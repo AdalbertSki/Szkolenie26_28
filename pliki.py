a5018af89d0a2770b4ca55dbe0e095ca2f1e5068
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-28 09:51:45 +0200

    pliki

--- pliki.py
+++ pliki.py
@@ -4,3 +4,18 @@
 #    print(linia.strip())  # strip obcina zbędne elementy puste pola
 # finally:
 #   plik.close()
+
+try:
+    plik = open('baza444.txt', "r")
+    for linia in plik:
+        print(linia.strip())
+    plik.close()
+except FileNotFoundError:
+    print("Zła nazwa pliku, spróbuj ponownie...")
+finally:
+    for i in range(5):
+        print(i + 1 * 2)
+
+with open('baza.txt') as p:
+    for linia in p:
+        print(linia.strip())
