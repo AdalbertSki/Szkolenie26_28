9f7478a5d00eefea7f3c7e46b9bd9f2c9497ae6b
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-28 14:44:03 +0200

    klasy

--- piatek/klasa1.py
+++ piatek/klasa1.py
@@ -22,19 +22,44 @@ class Dom:
     #     print("Ilość okien wynosi: ", self.ilosc_okien)
 
 
-class Dom2:
+# class Dom2:
+#     '''
+#     To jest klasa DOM2, która sprawdza działanie
+#     OOP w Pythonie...
+#     '''
+#     def __init__(self, metraz, kolor, ilosc_okien):
+#         self.metraz = metraz
+#         self.kolor = kolor
+#         self.ilosc_okien = ilosc_okien
+#     def zmien_metraz(self):
+#         wybor = int(input("Podaj metraż "))
+#         self.metraz = wybor
+#         print("Metraż wynosi: ", self.metraz)
+#
+#     def zmien_kolor(self):
+#         wybor = input("Podaj kolor ")
+#         self.kolor = wybor
+#         print("Kolor wynosi: ", self.kolor)
+#
+#     def zmien_okna(self):
+#         wybor = int(input("Podaj ilość okien "))
+#         self.ilosc_okien = wybor
+#         print("Ilość okien wynosi: ", self.ilosc_okien)
+
+
+class Dom3:
     '''
-    To jest klasa DOM2, która sprawdza działanie
+    To jest klasa DOM3, która sprawdza działanie
     OOP w Pythonie...
     '''
     def __init__(self, metraz, kolor, ilosc_okien):
-        self.metraz = metraz
+        self.__metraz = metraz
         self.kolor = kolor
         self.ilosc_okien = ilosc_okien
     def zmien_metraz(self):
         wybor = int(input("Podaj metraż "))
-        self.metraz = wybor
-        print("Metraż wynosi: ", self.metraz)
+        self.__metraz = wybor
+        print("Metraż wynosi: ", self.__metraz)
 
     def zmien_kolor(self):
         wybor = input("Podaj kolor ")
