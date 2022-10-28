a76e513c43836d90d6d65d61f69eedd563f5febd
SkibiWoj <skibinski.wojciech@gmail.com>
2022-10-28 12:40:58 +0200

    json

--- jsonek.py
+++ jsonek.py
@@ -3,15 +3,16 @@ import json
 person = '{"name": "Bob", "languages": ["English", "French", ["Python", "Java"]]}'
 person_dict = json.loads(person)
 
-print(person_dict)
-print(person_dict['languages'])
-print(person_dict['languages'][0])
-print(person_dict['languages'][1])
-print(person_dict['languages'][2][0])
+# print(person_dict)
+# print(person_dict['languages'])
+# print(person_dict['languages'][0])
+# print(person_dict['languages'][1])
+# print(person_dict['languages'][2][0])
 
-with open('person.json', 'r') as f:
-    data = json.load(f)
-print(data)
+# with open('person.json', 'r') as f:
+#     data = json.load(f)
+# print(data)
+
 
 
 
